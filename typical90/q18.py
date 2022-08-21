3a9f44fe3095a3ce0657a68a42a883c35e7fd7e0
Yuichi Semura <semura.yuichi@gmail.com>
2022-08-21 20:56:23 +0900

    typiecal18, 20, 21

---
+++ typical90/q18.py
@@ -0,0 +1,19 @@
+import math
+
+def main():
+
+    T = int(input())
+    L, X, Y = map(int, input().split())
+    Q = int(input())
+    xx = X ** 2
+    for _ in range(Q):
+        e = int(input())
+        rad = ( e % T ) / T * math.pi * 2
+        yt = - L / 2 * math.sin(rad)
+        zt = L / 2 * (1 - math.cos(rad))
+        yy = xx + (yt - Y) ** 2
+        zz = yy + zt ** 2
+        print(math.degrees(math.acos(math.sqrt(yy) / math.sqrt(zz))))
+
+if __name__ == '__main__':
+    main()
---
+++ typical90/q21.py
@@ -0,0 +1,20 @@
+def main():
+
+    from collections import defaultdict
+    n, m = map(int, input().split())
+    abl = [map(int, input().split()) for _ in range(m)] 
+    v = [defaultdict(int) for _ in range(n)]
+    
+    
+    for a, b in abl:
+        v[a-1][str(b-1)] += 1
+    print(v)
+    result = 0
+    for x, dic in enumerate(v):
+        for y, count in dic.items():
+            print(x, y, count, v[int(y)][str(x)])
+            result += count * v[int(y)][str(x)]
+    print(result)
+
+if __name__ == '__main__':
+    main()
